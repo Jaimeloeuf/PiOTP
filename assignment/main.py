97ee34dca79f330db2aed53577e3042596c9b6a6
Jaime Loeuf <jaimeloeuf@gmail.com>
2018-12-10 17:32:04 +0800

    Changes: 1.	Added many more routes to the API that need to be further developed.
    2.	Added a basic async timer/wait function
    
    3.	Added more stuff to the State class and created a status object
    
    Note that all the code has yet to be tested. This is just a commit to keep
    up with the changes mid way before finnishing up.

--- assignment/main.py
+++ assignment/main.py
@@ -10,12 +10,18 @@ Program flow:
     2.3.  Quick fire buzzer for 2 seconds
     2.4.  Stop everything after 5 seconds
 3.  Echo the JSON data back to the client if handler is called because of the POST request
+
+
+buzz function will be called at every action
 """
 
 # Dependencies
+# Server dependencies
 from flask import Flask, render_template, request, abort
+# Raspberry Pi GPIO usage dependencies
 from gpiozero import LED, PWMLED, Button
-from time import sleep
+# import asyncio
+from asyncio import sleep
 
 # Setup
 app = Flask(__name__)
@@ -23,45 +29,108 @@ app = Flask(__name__)
 # Create an object to maintain the LED state
 class State:
     # Assigning default states
-    led = [LED(27), LED(22)]  # Using GPIO pin 16 for LED
-	pwmLED = PWMLED(17)  # Using GPIO pin 17 for PWM on LED
-	buzzer = LED(21)  # Using GPIO pin 10 for Buzzer
-	btn = Button(23)  # Using GPIO pin 3 for Button input
-	def __init__(self, led, buzz, btn):
-		self.btn = btn
+    # Using GPIO pin 16 for LED
+    pwmLED = PWMLED(17)  # Using GPIO pin 17 for PWM on LED
+    # Using GPIO pin 10 for Buzzer
+    # Using GPIO pin 3 for Button input
+    leds = {
+        green: LED(22),
+        yellow: LED(27),
+        red: LED(27),
+    }
 
-    def blinkLED():
+    def __init__(self, leds=[LED(27), LED(22)], buzz=LED(21), btn=Button(23)):
+        self.leds = leds
+        self.buzz = buzz
+        self.btn = btn
 
-	def fadeLED():
-		# Create a async timer
-		pwmLED.pulse()  # Fade in/out the Red led
-	def buzz():
-		buzzer.on()
-		# Create a async timer
-		buzzer.off()
+    def LED(self, blink='blink', led=None):
+        if blink == 'blink':
+            # Blink a specific led in the array of leds if the position is specified in the led variable
+            if led != None:
+                self.leds[led].blink()
+            else:
+                # Else make all leds in the array blink
+                for led in self.leds:
+                    led.blink()
+        elif blink == 'stop'
 
+    def fadeLED(self):
+        self.pwmLED.pulse()  # Fade in/out the Red led
+    def buzz(self):
+        self.buzzer.on()
+        await sleep(1.5) # Create an async wait timer
+        self.buzzer.off()
+
+# 'Global' object that maintains state
+state = State()
+
+
+# Status object that will be serialized into JSON before being sent back to the client
+# Below is the initial state of the program
+status = {
+    leds: {
+        green: 'off',
+        yellow: 'off',
+        red: 'off'
+    },
+    pwmLED: {
+
+    },
+    buzzer: 'off'
+}
 
     # Flask server routes
 @app.route('/', methods=['GET'])
 def index():
     return render_template('./index.html')
 
+# led APIs
+@app.route('/api/green/get', methods=['GET'])
+def index():
 
-@app.route('/api/pressed', methods=['POST'])
+@app.route('/api/yellow/get', methods=['GET'])
+def index():
+
+@app.route('/api/red/get', methods=['GET'])
+def index():
+
+
+
+@app.route('/api/green/on', methods=['POST'])
+def green():
+    return render_template('./index.html')
+
+@app.route('/api/yellow/on', methods=['POST'])
+def yellow():
+    return render_template('./index.html')
+
+@app.route('/api/red/on', methods=['POST'])
+def red():
+    return render_template('./index.html')
+
+
+@app.route('/api/green/off', methods=['POST'])
+def green():
+    return render_template('./index.html')
+
+@app.route('/api/yellow/off', methods=['POST'])
+def yellow():
+    return render_template('./index.html')
+
+@app.route('/api/red/off', methods=['POST'])
+def red():
+    return render_template('./index.html')
+
+
+# Route to mimic pressing the button
+@app.route('/api/press_btn', methods=['POST'])
 def get_data():
-    handler()
     data = request.get_json()
     hello = data['Hi']
     return hello
 
-# RPI GPIO related
-
-def handler():
-    led[0].blink(0.3, 0.3)
-    led[1].blink(0.5, 0.5)
-    buzzer.on()
-    sleep(1)
-    buzzer.off()
+# leds[1].blink(0.5, 0.5)
 
 btn.when_pressed = handler # Setting event listeners for Button input change
 
