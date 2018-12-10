666afdb5f951e31c4fc5769df7bfcc0f14222840
Jaime Loeuf <jaimeloeuf@gmail.com>
2018-12-10 20:52:28 +0800

    Changes: 1.	Update the API routes thing to use variables in the URL, so I do not need 	to define individual routes for every single LED/Buzzer/Action
    2.	Moved the initial led state into the __init__/constructor function's
    	parameter definition instead of having it declared seperately in the class.
    
    3.	Renamed the variable that holds the reference to the buzzer's controller
    	method from 'buzz' to 'buzzer' to prevent conflict with the method name
    	from the same Class.
    
    4.	Updated the 'set_led' and 'buzz' method in the state Class to take in more
    	function arguements to make these methods more general purpose ...
    	Meaning that for example, I can now use the same function to blink and to
    	on or to off the led, instead of just having it blink. Other features like
    	specifying to on or off the buzzer or to set it to play for a second and more.
    
    5.	Updated the status object of to use the GPIOzero's built in method calls to
    	set the state of the variables, instead of doing it manually.
    	More work to do to make this auto-monitor for changes to the system state.

--- assignment/main.py
+++ assignment/main.py
@@ -17,7 +17,7 @@ buzz function will be called at every action
 
 # Dependencies
 # Server dependencies
-from flask import Flask, render_template, request, abort
+from flask import Flask, render_template, request, abort, jsonify
 # Raspberry Pi GPIO usage dependencies
 from gpiozero import LED, PWMLED, Button
 # import asyncio
@@ -25,59 +25,76 @@ from asyncio import sleep
 
 # Setup
 app = Flask(__name__)
+    # pwmLED = PWMLED(17)  # Using GPIO pin 17 for PWM on LED
 
 # Create an object to maintain the LED state
 class State:
     # Assigning default states
-    # Using GPIO pin 16 for LED
-    pwmLED = PWMLED(17)  # Using GPIO pin 17 for PWM on LED
-    # Using GPIO pin 10 for Buzzer
-    # Using GPIO pin 3 for Button input
-    leds = {
-        green: LED(22),
-        yellow: LED(27),
-        red: LED(27),
-    }
-
-    def __init__(self, leds=[LED(27), LED(22)], buzz=LED(21), btn=Button(23)):
+    # Using GPIO pin 10 for Buzzer # Using GPIO pin 3 for Button input
+    def __init__(self, leds={ green: LED(22), yellow: LED(27), red: LED(27) }, buzzer=LED(21), btn=Button(23)):
         self.leds = leds
-        self.buzz = buzz
+        self.buzzer = buzzer
         self.btn = btn
 
-    def LED(self, blink='blink', led=None):
+    def set_led(self, blink='blink', led=None):
         if blink == 'blink':
-            # Blink a specific led in the array of leds if the position is specified in the led variable
+            # Blink a specific led in the array of leds if a led is specified by the 'led' variable
             if led != None:
                 self.leds[led].blink()
             else:
                 # Else make all leds in the array blink
                 for led in self.leds:
                     led.blink()
-        elif blink == 'stop'
-
+        elif blink == 'on'
+            # Off a specific led in the array of leds if a led is specified by the 'led' variable
+            if led != None:
+                self.leds[led].off()
+            else:
+                # Else off all leds in the array
+                for led in self.leds:
+                    led.off()
+        elif blink == 'off'
+            # Off a specific led in the array of leds if a led is specified by the 'led' variable
+            if led != None:
+                self.leds[led].off()
+            else:
+                # Else off all leds in the array
+                for led in self.leds:
+                    led.off()
+        else:
+            print('Error with method call to LED:\nUnrecognised input arguement for "blink" parameter')
     def fadeLED(self):
         self.pwmLED.pulse()  # Fade in/out the Red led
-    def buzz(self):
-        self.buzzer.on()
-        await sleep(1.5) # Create an async wait timer
-        self.buzzer.off()
+    def buzz(self, set=None):
+        # If 'set' is set, use it to manually control state. Else do a auto buzz
+        if set != None:
+            if set == 'on':
+                self.buzzer.on()
+            elif set == 'off':
+                self.buzzer.off()
+            else:
+                print('Error with method call to buzz:\nUnrecognised input arguement for "set" parameter')
+        else:
+            self.buzzer.on()
+            await sleep(1.5) # Create an async wait timer
+            self.buzzer.off()
+
 
 # 'Global' object that maintains state
 state = State()
-
 
 # Status object that will be serialized into JSON before being sent back to the client
 # Below is the initial state of the program
 status = {
     leds: {
-        green: 'off',
-        yellow: 'off',
-        red: 'off'
+        green: state.leds.green.is_lit(),
+        yellow: state.leds.yellow.is_lit(),
+        red: state.leds.red.is_lit()
     },
     pwmLED: {
 
     },
-    buzzer: 'off'
+    buzzer: state.buzz.is_lit(),
 }
 
     # Flask server routes
@@ -85,54 +102,41 @@ status = {
 def index():
     return render_template('./index.html')
 
+
+@app.route('/api/get/state', methods=['GET'])
+def current_state():
+    # return jsonify({ 'State': status })
+    return jsonify(status)
+
+
 # led APIs
-@app.route('/api/green/get', methods=['GET'])
-def index():
-
-@app.route('/api/yellow/get', methods=['GET'])
-def index():
-
-@app.route('/api/red/get', methods=['GET'])
-def index():
+@app.route('/api/<color>/get', methods=['GET'])
+def index(color):
 
 
-
-@app.route('/api/green/on', methods=['POST'])
-def green():
-    return render_template('./index.html')
-
-@app.route('/api/yellow/on', methods=['POST'])
-def yellow():
-    return render_template('./index.html')
-
-@app.route('/api/red/on', methods=['POST'])
-def red():
+@app.route('/api/<color>/<state>', methods=['POST'])
+def set_state(color, state):
+    # Smth like react, where I set the state variable. Which is actually just a 'shadow state', which will look at the diff and update the real state
     return render_template('./index.html')
 
 
-@app.route('/api/green/off', methods=['POST'])
-def green():
-    return render_template('./index.html')
 
-@app.route('/api/yellow/off', methods=['POST'])
-def yellow():
-    return render_template('./index.html')
-
-@app.route('/api/red/off', methods=['POST'])
-def red():
-    return render_template('./index.html')
+def btn_pressed_handler():
+    state.set_led()
+    await state.buzz()
 
 
 # Route to mimic pressing the button
 @app.route('/api/press_btn', methods=['POST'])
 def get_data():
+    btn_pressed_handler()
+    # Below is a simple echo function
     data = request.get_json()
-    hello = data['Hi']
-    return hello
+    return data
 
 # leds[1].blink(0.5, 0.5)
 
-btn.when_pressed = handler # Setting event listeners for Button input change
+btn.when_pressed = btn_pressed_handler # Setting event listeners for Button input change
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000, debug=True)
