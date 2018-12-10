17aa47fa408fdfabeb79ac40c836d44480f16da3
Jaime Loeuf <jaimeloeuf@gmail.com>
2018-12-11 00:54:50 +0800

    Changes: 1.	Moved the btn_pressed_handler to become a method of the State Class's object. 	So now the State object also exposes a behaviour to object user.
    2.	Removed the import statement for asyncio in the 'main' file as it is no
    	longer needed as a dependency.
    
    	Moved the dependency requirement over to the pi_state module.
    
    3.	Minor code clean up for the main module, following all the code updates.
    
    4.	Created an async function to deal wth offing LEDs in the pi_state module, to
    	help keep the Class code modularised.
    	The Object's set_led and buzz method now accepts an additional parameter in
    	seconds. If set to more than 0, the led or buzzer will on for set amount of
    	time before turning off, in an asynchronous method, meaning non-blocking code.
    	I can now use this to set a variadic on time.
    
    5.	Fixed all the errors in the pi_state module where the dictionaries were
    	accidentally declared/created wrongly with JavaScript syntax.
    
    6.	Created module 'checking' code inside the pi_state module to make sure that
    	the module is not runned as a standalone or main module.
    	The code will complain about the error and kill the process.
    
    Note:
    	All of the above code changes has yet to be tested to be working.
    	However no errors from linter as of now.

--- assignment/main.py
+++ assignment/main.py
@@ -3,7 +3,7 @@ Description:
 Code for IOTP Assignment
 
 Program flow:
-1.  Wait for button to be pressed.  /  Wait for a POST requests at the /api/pressed route
+1.  Wait for button to be pressed.  /  Wait for a POST requests at the /api/press_btn route
 2.  Call handler function to respond
     2.1 Red LED fades with PWM.
     2.2.  Yellow and Green LED Blinks
@@ -14,18 +14,16 @@ Program flow:
 # Dependencies
 # Server dependencies
 from flask import Flask, render_template, request, abort, jsonify
-# import asyncio
-from asyncio import sleep
 # Import the state controller from pi_state module that I wrote
-from pi_state import State, btn_pressed_handler
+from pi_state import State
 
-# 'Global' object that maintains state
+# 'Global' object that holds a State object thath maintains state of the RaspPi's GPIO pins
 state = State()
 # 'Global' object for Flask server
 app = Flask(__name__)
 
-
 # Flask server routes
+
 @app.route('/', methods=['GET'])
 def index():
     return render_template('./index.html')
@@ -34,19 +32,19 @@ def index():
 def current_state():
     # The returned 'state' object will be serialized into JSON before being sent back to the client
     # return jsonify({ 'State': status })
-    return jsonify(status)
+    return jsonify(state.get())
 
 # led APIs
 @app.route('/api/led/<color>/get', methods=['GET'])
-def index(color):
+def get_led_state(color):
     if state.leds[color] != None:
         return jsonify({ color: state.leds[color] })
 
 # Smth like react, where I set the state variable. Which is actually just a 'shadow state', which will look at the diff and update the real state
 @app.route('/api/led/<color>/set/<state>', methods=['POST'])
-def set_state(color, state):
+def set_led_state(color, state):
     if state.leds[color] != None:
-        if state.set_led(state=state, led=color)
+        if state.set_led(state=state, led=color):
             return jsonify({ color: state.leds[color] })
         else:
             return 'Invalid request failed'
@@ -54,7 +52,7 @@ def set_state(color, state):
 # Flask Route to mimic pressing the button
 @app.route('/api/press_btn', methods=['POST'])
 def get_data():
-    btn_pressed_handler()
+    state.btn_pressed_handler()
     # Below is a simple echo function
     data = request.get_json()
     return data
--- assignment/pi_state.py
+++ assignment/pi_state.py
@@ -1,47 +1,70 @@
 # Raspberry Pi GPIO usage dependencies
 from gpiozero import LED, PWMLED, Button
+# import asyncio to allow async sleep/delay operation
+from asyncio import sleep
 
-# Create an object to maintain the LED state
+async def off_leds(leds, onTime):
+    if onTime > 0:
+        await sleep(onTime) # Create an async wait timer
+        if isinstance(leds, list):
+            # Off leds one by one if it is a list that was passed in
+            for led in leds:
+                led.off()
+        else:
+            leds.off()
+
+# Class to maintain the state of the Pi's GPIO pins
 class State:
-    # Assigning default states
-    # Using GPIO pin 10 for Buzzer # Using GPIO pin 3 for Button input
-    def __init__(self, leds={ green: LED(22), yellow: LED(27), red: LED(27) }, buzzer=LED(21), btn=Button(23)):
+    # Default states: GPIO pin 10 for Buzzer ; GPIO pin 3 for Button input ..etc..
+    def __init__(self, leds={ 'green': LED(22), 'yellow': LED(27), 'red': LED(27) }, buzzer=LED(21), btn=Button(23)):
         self.leds = leds
         self.buzzer = buzzer
+        btn.when_pressed = self.btn_pressed_handler # Setting event listeners for Button input change
         self.btn = btn
 
-    def set_led(self, state='blink', led=None):
+    async def set_led(self, state='blink', led=None, onTime=0):
         if state == 'on':
             # Off a specific led in the array of leds if a led is specified by the 'led' variable
             if led != None:
-                self.leds[led].off()
+                self.leds[led].on()
+                await off_leds(self.leds[led], onTime)
             else:
                 # Else off all leds in the array
                 for led in self.leds:
-                    led.off()
-        elif state == 'off'
-            # Off a specific led in the array of leds if a led is specified by the 'led' variable
-            if led != None:
-                self.leds[led].off()
-            else:
-                # Else off all leds in the array
-                for led in self.leds:
-                    led.off()
+                    led.on()
+                await off_leds(self.leds, onTime)
         elif state == 'blink':
             # Blink a specific led in the array of leds if a led is specified by the 'led' variable
             if led != None:
                 self.leds[led].blink()
+                await off_leds(self.leds[led], onTime)
             else:
                 # Else make all leds in the array blink
                 for led in self.leds:
                     led.blink()
+                await off_leds(self.leds, onTime)
+        elif state == 'off':
+            # Off a specific led in the array of leds if a led is specified by the 'led' variable
+            if led != None:
+                self.leds[led].off()
+            else:
+                # Else off all leds in the array
+                for led in self.leds:
+                    led.off()
         else:
             print('Error with method call to LED:\nUnrecognised input arguement for "state" parameter')
             return False # Indicate operation failed
         return True # Indicate operation succeded
-    def fadeLED(self):
-        self.pwmLED.pulse()  # Fade in/out the Red led
-    def buzz(self, state=None):
+
+    def fadeLED(self, led):
+        if self.leds[led] == None:
+            print('Error with method call to fadeLED:\nUnrecognised input arguement for "led" parameter')
+            return False
+        # Convert the led into a PWM Led
+        # self.leds[led].PWMLED
+        # self.pwmLED.pulse()  # Fade in/out the Red led
+
+    async def buzz(self, state=None, onTime=0):
         # If 'state' is set, use it to manually control state. Else do a auto buzz
         if state != None:
             if state == 'on':
@@ -52,34 +75,43 @@ class State:
                 print('Error with method call to buzz:\nUnrecognised input arguement for "state" parameter')
         else:
             self.buzzer.on()
-            await sleep(1.5) # Create an async wait timer
+            await sleep(onTime if (onTime > 0) else 1.5) # Create an async wait timer with 1.5 default wait time
             self.buzzer.off()
+
     def get(self, state_of='all'):
         # Construct state based on input arguement and return it to method caller
         if state_of == 'all':
-            state = {
-                leds: {
-                    green: self.leds.green.is_lit(),
-                    yellow: self.leds.yellow.is_lit(),
-                    red: self.leds.red.is_lit()
+            return {
+                'leds': {
+                    'green': self.leds.green.is_lit(),
+                    'yellow': self.leds.yellow.is_lit(),
+                    'red': self.leds.red.is_lit()
                 },
-                pwmLED: {
-
-                },
-                buzzer: state.buzz.is_lit(),
+                'buzzer': self.buzzer.is_lit()
             }
         elif state_of == 'leds':
-            state = {
-
+            return {
+                'green': self.leds.green.is_lit(),
+                'yellow': self.leds.yellow.is_lit(),
+                'red': self.leds.red.is_lit()
             }
-        return state
+        elif state_of == 'buzzer':
+            return { 'buzzer': self.buzzer.is_lit() }
+        else:
+            print('Error with method call to get:\nUnrecognised input arguement for "state_of" parameter')
 
-# Event handler for button pressed
-def btn_pressed_handler():
-    state.set_led()
-    await state.buzz()
+    # Event handler for button pressed
+    def btn_pressed_handler(self):
+        self.set_led() # To edit
+        await self.buzz()
+
+# Error checking code to prevent running this module as it is.
+if __name__ == "__main__":
+    print('Error, this module, "%s" should not be used as a standalone module' % __name__)
+    # print('Error, this module, "pi_state" should not be used as a standalone module')
+    exit()
+
+
 
 # leds[1].blink(0.5, 0.5)
 # pwmLED = PWMLED(17)  # Using GPIO pin 17 for PWM on LED
-
-btn.when_pressed = btn_pressed_handler # Setting event listeners for Button input change
