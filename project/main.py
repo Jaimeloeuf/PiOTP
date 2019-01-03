0d26f173b4dab65d58f08a39a6f1e5d60881dd49
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-01-03 23:57:02 +0800

    Changes: 1.	Created the templates folder for all the jinja templates and also created 	a index.html template inside the folder that will serve as the home page.
    2.	Updated the server to read PORT variable from environment and also added
    	some basic code to be further worked on for redirecting based on JWT presence.

--- project/main.py
+++ project/main.py
@@ -13,27 +13,41 @@ POST:
 """
 # Dependencies
 # Server dependencies
-from flask import Flask, render_template, request, abort, jsonify
+from flask import Flask, render_template, redirect, url_for, request, abort, jsonify
+# To get environmental variables avail to the process
+from os import environ
 
 # 'Global' object for Flask server
 app = Flask(__name__)
 
 # Flask server routes
+
+
 @app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def home_page():
-    # Use templating engine to put user data into the home page and send it back
-    return render_template('./index.html')
+	# Check if the user logged in already by looking for a valid JWT.
+
+	# If the user is not logged in
+	redirect(url_for('login'))
+    # Use put user data into home page and send it back
+    return render_template('./index.html', name = name)
 
 # Flask Route to mimic pressing the button
 @app.route('/login', methods=['POST'])
 def login():
+	if request.header.jwt
+		redirect(url_for('home')) # If user already logged in with valid JWT, then redirect to home page
+	# Call DB to check password
+	# If matches, create a JWT for the user
 	checkPasswd()
 
 # Server Route to test if remote server is online
 @app.route('/ping', methods=['GET'])
 def ping():
-	# Set status code to 200 and end response.
-	return 200
+	# Default 200 status code will be sent back
+	return
 	
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000, debug=True)
+	# Use PORT from the environment if defined, otherwise default to 5000.
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=True)
