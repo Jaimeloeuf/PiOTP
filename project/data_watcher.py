427eeb538b0901c2527ecf437294c40b750c581a
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-01-29 23:55:08 +0800

    Changes: 	In the data_watcher module, I am currently working on calling the callback 	functions in seperate threads, which are all ran in a seperate Process 	to allow concurrent execution in parallel. 	Currently in the testing phase for this new method.

--- project/data_watcher.py
+++ project/data_watcher.py
@@ -3,7 +3,11 @@
     - Perhaps a method to allow all the callback functions to be executed concurrently in multiple threads.
     - Implement a show all listener method
     - Implement a remove listener method
+        - By probably using a ID that is associated with the enumeration of the callback(s) array
 """
+
+from threading import Thread
+from multiprocessing import Process, process
 
 
 class watch:
@@ -14,6 +18,7 @@ class watch:
 
     def set(self, data):
         self.__data = data
+        print('Set function called')
         self.__event()
         return self
 
@@ -29,10 +34,34 @@ class watch:
         self.__cbs.clear()  # Not sure if this method works, needs to be tested
         return self
 
-    def __event(self):
-		# Should call all the callbacks in sperate threads/processes
+    def __event2(self):
+        # Should call all the callbacks in sperate threads/processes
         for cb in self.__cbs:
             cb()
+
+    def __run_callbacks(self):
+        # create and start our threads
+        threads = []
+        # Loop through and run all the callbacks as seperate threads
+        for cb in self.__cbs:
+            thread = Thread(target=cb)
+            thread.daemon = True
+            threads.append(thread)
+            thread.start()
+
+        # wait for all the threads to finish using (join)
+        for thread in threads:
+            # Blocking call till all the threads are done.
+            thread.join()
+
+    def __event(self):
+        # All the callback functions should be pure functions with no external scoped dependencies
+        cb_process = Process(target=self.__run_callbacks)
+        cb_process.daemon = True
+        cb_process.start()
+
+        # Do I need to join??
+        cb_process.join()
 
 
 # If this module is called as a standalone module to run, then execute the example code
@@ -60,19 +89,38 @@ if __name__ == "__main__":
     sensorData.addListener(hi3)
 
     # Add a time delay to simulate real life operations
-    sleep(4)
-
+    sleep(2)
     # Update the data in the object, this will cause all the callbacks to be called.
     sensorData.set(5)
     # Print out the updated value stored in the object.
     print(sensorData.get())
 
     # Add a time delay to simulate real life operations
-    sleep(4)
-
-# Trigger when set function called, or when the valus is set, or when the value different from the value inside now
-
+    sleep(2)
     # Update the data in the object, this will cause all the callbacks to be called.
     sensorData.set(5)
     # Print out the updated value stored in the object.
     print(sensorData.get())
+
+    while True:
+        pass
+
+    """ The first time u "set" a value, or should I say, watch the value, nothing happens at all. No callback, nothing.
+        However when u set the variable with the exact same value, it still runs all the callbacks. Is this good or is this bad?
+        Should data watcher's callbacks run when the value is changed, or when set method is called??
+        
+        So when I use this the publish data thing, when the data is still the same, do I want the data to be published?
+        
+        OH!!
+        
+        Maybe I can have 2 types of callbacks!
+        a proprety created by decoroators and
+        
+        one called on_set and one called on_change
+        
+        then u can do something like,
+        data.on_set += cb
+        data.on_change += cb
+
+
+        """
