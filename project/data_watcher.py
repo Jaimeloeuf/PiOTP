709b706f0a655b2ee11303567ca9595187b9062f
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-02-01 12:08:06 +0800

    Changes: 1.	Reverted back to a previous commit for data_watcher module due to some 	errors and fixed them in this commit together with the changes.
    2.	Have fully implemented the seperation on_change and on_set callback
    	functions on calling the set method of the watch data object.
    
    Note:
    	Will be moving this data_watcher module into its own seperate package.

--- project/data_watcher.py
+++ project/data_watcher.py
@@ -1,90 +1,106 @@
-"""	@Todos
-    - Implement a method to allow user to change the sequence in which the listeners are executed in
-    - Perhaps a method to allow all the callback functions to be executed concurrently in multiple threads.
-    - Implement a show all listener method
-    - Implement a remove listener method
-        - By probably using a ID that is associated with the enumeration of the callback(s) array
-"""
-
 from threading import Thread
-import threading
-from multiprocessing import Process, process
 
 
 class Watch:
-    __cbs = []
+    # __on_set_cbs callback functions run the moment set method is used to set a value to the variable.
+    __on_set_cbs = []
+    # __on_change_cbs callback functions only run if the new value set is different from the previous value.
+    __on_change_cbs = []
 
     def __init__(self, data):
         self.__data = data
 
+    # Method to set a value to the data variable watched by this class
     def set(self, data):
-        self.__data = data
-        # self._event2()
-        self.__event()
+        # If set is called and data value has been changed, save the data and run on change callbacks
+        if data != self.__data:
+            self.__data = data
+            # Call all the __on_change_cbs callback functions
+            self.__event(self.__on_change_cbs)
+
+        # Regardless of data, call all the __on_set_cbs callback functions when set method called.
+        self.__event(self.__on_set_cbs)
+        # Return self reference to allow method call chainings.
         return self
 
-    def get(self):
+    # Decorated Method to use as an attribute to get watched/stored data
+    @property
+    def value(self):
         return self.__data
 
-    def addListener(self, cb):
-        self.__cbs.append(cb)
+    # Method to append a new callback function to be ran when the set method is called
+    def on_set(self, cb):
+        self.__on_set_cbs.append(cb)
+        # Return self reference to allow method call chainings.
+        return self
+
+    # Method to append a new callback function to be ran when the watched data is changed
+    def on_change(self, cb):
+        self.__on_change_cbs.append(cb)
+        # Return self reference to allow method call chainings.
         return self
 
     def removeListener(self, cb=None):
         # To implement the second part where only the specified callback function is removed.
-        self.__cbs.clear()  # Not sure if this method works, needs to be tested
+        # self.__cbs.clear()  # Not sure if this method works, needs to be tested
+        # Return self reference to allow method call chainings.
         return self
 
-    def __event(self):
+    def __event(self, callbacks):
         # Loop through and run all the callbacks as seperate threads
-        for cb in self.__cbs:
+        for cb in callbacks:
             Thread(target=cb, daemon=True).start()
 
 
 # If this module is called as a standalone module to run, then execute the example code
 if __name__ == "__main__":
+    # Dependencies for the example code.
     from time import sleep
+    import threading
 
     # Create a new data variable and store in the watchData object
     sensorData = Watch(36349)
-    # The data stored in the object can only be accessed via the get method
-    print(sensorData.get())
+    # The data stored in the object can only be accessed via the value property
+    print(sensorData.value)
 
     # Below are 3 different callbacks that should run when the data changes
-
     def hi():
-        sleep(4)
+        sleep(2.5)
         print('hello world')
 
-    # Add the callbacks to the object
-    sensorData.addListener(hi)
-
     def chicken():
-        print('Inner func as event handler')
-    sensorData.addListener(chicken)
+        sleep(0.2)
+        print('Chicken nuggets')
+
+    def on_change_cb():
+        print('The value has been changed')
+
+    # Add the callbacks to the object
+    sensorData.on_set(hi)
+    sensorData.on_set(chicken)
+    sensorData.on_change(on_change_cb)
 
     # Add a time delay to simulate real life operations
     sleep(1.4)
     # Update the data in the object, this will cause all the callbacks to be called.
-    sensorData.set(5)
+    sensorData.set(1)
     # Print out the updated value stored in the object.
-    print(sensorData.get())
+    print(sensorData.value)
 
     # Add a time delay to simulate real life operations
     sleep(1.4)
     # Update the data in the object, this will cause all the callbacks to be called.
-    sensorData.set(5)
+    sensorData.set(2)
     # Print out the updated value stored in the object.
-    print(sensorData.get())
+    print(sensorData.value)
 
+    # Add a time delay to simulate real life operations
     sleep(1.4)
+    # Update the data in the object, this will cause all the callbacks to be called.
+    sensorData.set(2)
+    # Print out the updated value stored in the object.
+    print(sensorData.value)
 
-    # Using __ double underscore to hide the attribute through attribute name mangling, to prevent user from directly modifying it.
-    # print(sensorData.__cbs)  # Output: Err: no attr.
-    # print(sensorData._Watch__cbs) # Output: [the callback functions, ...]
-
-    # The enumerate staticmethod returns a list of thread names of all the threads that are still alive.
-    # print(threading.enumerate())
 
     # Function that waits for all the daemon threads to end by calling join method on them.
     def daemon_thread_join():
@@ -92,26 +108,5 @@ if __name__ == "__main__":
         for thread in threading.enumerate():
             if thread.daemon:
                 thread.join()
-            
+
     daemon_thread_join()
-
-
-    """ The first time u "set" a value, or should I say, watch the value, nothing happens at all. No callback, nothing.
-        However when u set the variable with the exact same value, it still runs all the callbacks. Is this good or is this bad?
-        Should data watcher's callbacks run when the value is changed, or when set method is called??
-        
-        So when I use this the publish data thing, when the data is still the same, do I want the data to be published?
-        
-        OH!!
-        
-        Maybe I can have 2 types of callbacks!
-        a proprety created by decoroators and
-        
-        one called on_set and one called on_change
-        
-        then u can do something like,
-        data.on_set += cb
-        data.on_change += cb
-
-
-        """
--- project/web app/server.py
+++ project/web app/server.py
@@ -69,6 +69,7 @@ def ping():
 def generate_hash(password):
     return sha256.hash(password)
 
+
 def verify_hash(password, hash):
     return sha256.verify(password, hash)
 
