23d8f328b1f92a71c6b57d46a3223c8dd4399bf8
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-02-05 16:27:02 +0800

    Changes: 1.	Updated the AC module. 	- Updated the Publisher Object's topic to include the Topic Prefix. 	- Fixed the state method 	- Fixed the on method that uses the state method too. 	- Fixed the off method. 	- Created a new toggle method to allow user to toggle the state of the 	  AC module without calling on or off directly. 	- Wrote some test code inside the __main__ checking block to demo the 	  AC module's functionality. 	- Updated the use of Watch Class inside this module, created and added 	  a callback function when the data is changed. 	- Added more Docs
    2.	Deleted the old ac_tester module as there was no use for it anymore.

--- project/ac.py
+++ project/ac.py
@@ -3,7 +3,9 @@
 from gpiozero import LED, PWMLED, Button
 # Timer object to allow task scheduling that runs on another thread
 from threading import Timer
+# Watch Class used to watch the AC-Controller from tampering
 from Jevents import Watch
+# JQTT's Publisher Class used to contruct a Publisher for AC state.
 from JQTT import Publisher
 
 
@@ -18,19 +20,24 @@ from JQTT import Publisher
         ac.off()
     4. On ac for a set time asynchrounously.
         ac.on(1000)
+    5. Toggle the AC's state
+        ac.toggle()
+    6. Get the current state of the AC module as a Boolean value
+        ac.state
 """
 
 
 class acController:
-    # Single timer object to keep track of time.
-    ac_timer = None
+    """ Class used to build an AC controller object """
 
     def __init__(self, pin=27):
         """ Takes in optional input for GPIO Pin to use for the 'AC' module """
         # Call set_pin method to set and use default pin
         self.set_pin(pin)
-        # Create a publisher for the 'ac_state' topic
-        self.__pub = Publisher('ac_state')
+        # Create a instance variable to store Timer Objects
+        self.ac_timer = None
+        # Create a publisher for the 'ac_state' topic with the Topic prefix.
+        self.__pub = Publisher(('IOTP/grp4/channel/' + 'ac_state'))
 
     # Method to set pin to be connected to the buzzer other than the default one.
     def set_pin(self, pin=27):
@@ -38,16 +45,26 @@ class acController:
         # Create digital output with the LED function using pin input arguement
         self.aircon = LED(pin)
 
+    # Method to get current state of the AC. Can be used as a property
+    @property
+    def state(self):
+        # Strictly returns only a value of bool type
+        return self.aircon.is_active
+
     # Method to on the aircon, assuming that the aircon is active high output.
     def on(self, time_on=0):
+        # If the aircon is on, and is set to off itself with a Timer
+        if self.state and self.ac_timer != None:
+            # Stop the timer
+            self.ac_timer.cancel()
+
         # On the aircon if not on already
-        if self.state() == 'off':
+        if not self.state:
             self.aircon.on()
-            # Publish the new state
-            self.__pub += 'on'  # Pub using the magic method
+            # Publish the new state using the magic method
+            self.__pub < 'on'
 
         # If an on time is specified, wait asynchronously and off the aircon
-        # if time_on != None and self.ac_timer != None:
         if time_on:
             # If there is any timer stored previously, stop and overwrite it.
             if self.ac_timer != None:
@@ -59,24 +76,62 @@ class acController:
     # Wrapper method over the aircon off function.
     def off(self):
         # Off the aircon if currently on
-        if self.state() == 'on':
+        if self.state:
             self.aircon.off()
-            # Publish the new state
-            self.__pub += 'off'  # Pub using the magic method
+            # Publish the new state using the magic method
+            self.__pub < 'off'
 
-    # Method to get current state of the AC. Can be used as a property
-    @property
-    def state(self):
-        self.aircon.state()
+    # Method to toggle the current state of the aircon
+    def toggle(self):
+        # If there is a Timer in the background running that may interfere with this operation
+        if self.ac_timer != None:
+            # Stop the timer
+            self.ac_timer.cancel()
+        # Toggle the current state of the AC
+        self.aircon.toggle()
+        # Publish the new state using the magic method
+        self.__pub < 'on' if self.state else 'off'
+        # Return the state of the AC after toggling
+        return self.state
 
+    # Magic method to get the current state of the AC module
+    __repr__ = state
+
+
+# Callback function called when the AC controller is changed.
+def ac_con_changed():
+    print('ERR: AC controller changed.')
+    # Should implement the data watcher that uses a loop to constantly hash the object
+    # To check for change since this current method relies on the set method being called.
+
+# Put the 'global' ac variable on watch, to prevent other modules or anything from changing its reference without notice
+ac_Watcher = Watch(acController())
 
 # Variable that will be exported out to other modules to use to control the AC.
-# Put the 'global' ac variable on watch, to prevent other modules or anything from changing its reference without notice
-ac = Watch(acController()).value
+# Give the ac variable the actual AC controller object
+ac = ac_Watcher.value
 
-# Error checking code to prevent running this module as it is.
+# Set a callback to the Watched object
+ac_Watcher.on_set += ac_con_changed
+
+
 if __name__ == "__main__":
-    print('Error, this module, "%s" should not be used as a standalone module' % __name__)
-    exit()
+    # Example/Test code to run
+    from time import sleep
+    
+    # On the ac
+    ac.on()
+    sleep(2)
+    
+    # Off the ac
+    ac.off()
+    sleep(2)
 
-    # Should change this to run a test or example code instead
+    # On the ac, and set it to off after 3 seconds. Note that this is a non-blocking call
+    ac.on(3)
+    sleep(5)
+
+    # Toggle the current state back to On again
+    ac.toggle()
+    # Display the current state of the AC module. Should be 'True' here
+    print(ac.state)
--- project/ac_tester.py
+++
@@ -1,1 +0,0 @@
-# Write a test to see if I can still access the 'protected ac'
