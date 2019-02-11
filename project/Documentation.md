65c17766a86d3a8d311d140e50643737dd04d576
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-02-11 10:51:18 +0800

    Changes: 1.	Renamed the pi_controller2 to pi_controller module.
    2.	Renamed the original pi_controller module to pi_controller_extended_ver
    	for future feature extensions.
    
    3.	Updated Documentation file, cleaning it up and formatting it properly
    	and added missing informations.

--- project/Documentation.md
+++ project/Documentation.md
@@ -1,5 +1,3 @@
-You can only control the aircon by using the same reference to the one ac controller created at the start of the program. All modules who import it has access and the ability to control it.
-
 ### Main modules and their functions
 - pi_controller module:
 	This module is the 'glue' code that acts as the main control software on the Pi.
@@ -9,17 +7,17 @@ You can only control the aircon by using the same reference to the one ac contro
 	This module contains the Flask server, that is 'physical server' agnostic, and can run both
 	on the Pi as a seperate process, or on an external VPS.
 
-### Utilities libraries and modules
-- JQTT package:
-	A simple wrapper library/module over the single ended pub and sub actions provided by the paho MQTT package.
-- Jevents package:
-	A simple Events and Data observer library.
-- JSutils package:
-	A simple library that implements certain native JS features such as setInterval.
 
+### Utilities libraries, external dependencies and modules
+- [JQTT package](https://github.com/Jaimeloeuf/JQTT):
+	A simple wrapper library/module that provides a Publisher and Subscription module that can extends on the Client Class of the paho-mqtt package and also 2 wrapped single pub and sub actions functions.
+- [Jevents package](https://github.com/Jaimeloeuf/Jevents):
+	A simple Events and Data observer library used to implement data on change callback calling behaviours.
+- [JSutils package](https://github.com/Jaimeloeuf/JSutils):
+	A simple library used to emulate the setInterval native JavaScript feature.
 - ac module:
 	Defines and exposes the AC controller object and its methods, for us to set AC state.
-- BME module:
+- bme module:
 	Module used to read values (temp, humidity) from the BME Sensor.
 
 
@@ -28,6 +26,7 @@ You can only control the aircon by using the same reference to the one ac contro
 - ac: aircon
 - bz: buzzer
 - btn: button
+
 
 #### So what is the use case scenario?
 1.	Manual mode
@@ -73,31 +72,24 @@ The topic names are created by its unique name plus the prefix infront of it.
 		- Client application (either native or web)
 
 
+### How the Pi Controller works
+- Sensor data will be read repeatedly at a fixed interval independant of the current operating mode.
+- A setInterval loop will call the function repeatedly to update the sensor data variable, and everytime the variable updates, event handlers or callbackfunctions will be ran.
+- A global variable will maintain 'state'/record of the current operating mode.
+- A global variable stores the sensor data which is being 'Watched' to run event handlers
+- The pi_controller will publish the state of the AC everytime it changes.
+- All the function and global variables are defined and declared in the Pi Controller before they are used in the 'main' function of the module which lives in the 'if' block that checks if module ran as 'main'.
+- The aircon can only be controlled using the same reference to the one ac controller object created at the start of the program. No more than one ac object should be created from the acController class. All modules who import it has access and the ability to control it, but should not change its reference to prevent problems in other modules/packages.
 
+#### The different modes of the Pi Controller
+Since data is always read at the given interval, the init functions of the modes' just attach event handlers or callback functions to run when the variable is updated.
 
-The reading of data from the sensor is independant of the current mode. A setInterval loop will call the function repeatedly to update the variable. Everytime the variable updates, event handlers or callbackfunctions will be called.
-A global variable will maintain 'state'/record of the current operating mode.
-Make everything into data that is watched so everything will be like node js events
-	- Like the brokers and the topics should also be watched so when anything changes, there
-	can be event handlers, so you no longer need to set the publisher and broker every single time before publishing data
-	when the sensordata changes.
-
-The different modes
-	Data is always read at the given interval and the value is always updated after reading.
-	The function for all these modes will just be event handlers that will be called when the value is updated
-
-		sub to the broker's 'man' cmd topic,
-		given a new msg that the broker pushed down, do what is requested for
-			If the state of the AC is changed, publish the event too.
-
-Man:
-	# Do nth but publish the data, the pi_controller should not control the ac directly, only the incoming msg can
-	publish the sensor data to the broker
+- Manual mode:
+	- Do nth but publish the data and wait for incoming commands from the MQTT broker
 	--> repeat
 
-Auto:
-	publish the sensor data to the broker
-	Check if the sensor data exceeded the threshold variables
+- Auto mode:
+	- On every sensor data update, check if the sensor data exceeded the threshold variables
 		if exceeded
 			change AC state, should there be an on time given? to off the AC after a specified time
 			publish above event to broker
@@ -106,83 +98,58 @@ Auto:
 			publish abv event to broker
 	--> repeat
 
-Timezone:
-	publish the sensor data to the broker
+- Timed mode:
+	- Do nth but publish the data and wait for incoming commands from the MQTT broker
 	--> repeat
-
 	(in the background):
-	Timer object running, cos the AC is set to on, but with a timeout to off it.
+	Timer object running, where the AC is set to on, with a timeout using the timer object to off it.
 
+### List of possible valid messages that will be received from the MQTT broker
+- ac=on;
+	Turn the AC on regardless of current state.
+	When this command is received and the Pi is in the auto/timed mode
+		change the mode to manual
+		and turn ac on
 
+- ac=on;time=x;
+	Where x is the time that the AC will be on for.
+	Use ac'=off;' message to off the AC manually
 
-Everytime AC state changes, the pi_controller will publish by MQTT to the 'ac_state' topic.
+- ac=off;
+	When this command is received and the Pi is in the auto/timed mode
+		change the mode to manual
+		and turn ac off
 
+- mode=man;
+	Do not change the current state of the AC
+	Just change the mode to man, and disable all the auto controls to the AC
 
+- mode=auto;
+	Do not change the current state of the AC
+	Just change the mode to auto, and set the callbacks and everything and let it run
 
-
-====================================================================================================================================
-	List of possible valid messages that will be received from the MQTT broker
-
-Should I make all of the incoming messages url encoded? So I can just use any library or smth to parse
-	it for me instead of writing my own parser and switcher function.
-	Turn all of this into regex to search for incoming messages if not using the urlencoded method
-should there be different parsers based on which mode is currently operating?
-
-	ac=on;
-		Turn the AC on regardless of current state.
-		When this command is received and the Pi is in the auto/timed mode
-			change the mode to manual
-			and turn ac on
-
-	ac=on;time=x;
-		Where x is the time that the AC will be on for.
-		Use ac'=off;' message to off the AC manually
-
-	ac=off;
-		When this command is received and the Pi is in the auto/timed mode
-			change the mode to manual
-			and turn ac off
-
-	mode=man;
-		Do not change the current state of the AC
-		Just change the mode to man, and disable all the auto controls to the AC
-
-	mode=auto;
-		Do not change the current state of the AC
-		Just change the mode to auto, and set the callbacks and everything and let it run
-
-	mode=timed;
-		!!! Timed mode means, on for a set time, or on for a specific time of the day.
-		In the timed mode, another message will be expected
+- mode=timed;
+	!!! Timed mode means, on for a set time, or on for a specific time of the day.
+	In the timed mode, another message will be expected
 
 timed mode's actions can be interrupted by incoming messages that changes mode or ac state directly.
 In timed mode, the pi_controller will constantly wait for 2 different timed mode specific commands, to set a timeout value or a timeZone?
 
-	time=x;
-		Turn the AC on regardless of current state.
-		Create a timer to countdown with the given time to off the AC after that, if at anypoint, the
-		ac is offed, or this mode is 'turned off' then kill the timer
+- time=x;
+	Turn the AC on regardless of current state.
+	Create a timer to countdown with the given time to off the AC after that, if at anypoint, the
+	ac is offed, or this mode is 'turned off' then kill the timer
 
-	start=x;end=y;repeat=true;?
-		Disable everything and just wait for the start time, --> use a event thing or smth for the start time
-		when it is start time, on the AC and wait for the end time
-		when it is the end time, off the AC and put the system into timed mode.       doing nth and waiting for a new time?
-		The last kv pair received is optional and is used to indicate if the timed mode should constantly operate at the set time over and
-		over again through multiple days, if false or unset, then it will nvr repeat.
-
-
-mode is basically choosing which "pi controller" to use to control the pi's ac
-^ Somewhat true
+- start=x;end=y;repeat=true;?
+	Disable everything and just wait for the start time, --> use a event thing or smth for the start time
+	when it is start time, on the AC and wait for the end time
+	when it is the end time, off the AC and put the system into timed mode.       doing nth and waiting for a new time?
+	The last kv pair received is optional and is used to indicate if the timed mode should constantly operate at the set time over and
+	over again through multiple days, if false or unset, then it will nvr repeat.
 
 
-
-=============================================================================
 #### Misc Info:
 On using JavaScript in python directly using JS eval packages:
 - These can be used to implement the functions that I rewrote in native Python in the JSutils library, but did not due to the need of including additional dependencies and bloat.
 	- [PyMiniRacer](https://github.com/sqreen/PyMiniRacer)
 	- [Js2Py](https://pypi.org/project/Js2Py/)
-	
-
-
-Define all the global variable and functions first. Then at the bottom have a main function that uses them in one go... This means that I can import functions from outside modules
--- project/pi_controller_extended_ver.py
+++ project/pi_controller_extended_ver.py
@@ -3,17 +3,16 @@ Module Desciption:
     This module is the code that will be running on the Pi to glue together the operations
     of the MQTT Client lib, the AC controller and the BME sensor interface.
     User can pub a message to ask the pi_controller to pub a message about the current state.
-
-    Diff between pi controller 1 and 2 is that 2 does not have a timed mode.
 """
 
 # MQTT Client library
-from JQTT import Publisher, Subscription
+from JQTT import pub, sub, set_broker, set_topic, Publisher, Subscription
 # Get the AC controller from the ac module.
 from ac import ac
 # A data Watcher using the observer pattern
+# from Jevents import Watch
 from Jevents import Watch, wait_for_daemons
-from bme import get_temp
+# from BME import bme as BME
 # Import the setInterval class from JSutils package
 from JSutils import setInterval
 
@@ -24,7 +23,7 @@ operating_mode = Watch()
 # Global variable used to store sensorData wrapped in a Watch Class object.
 sensorData = Watch()
 # Global variable used to store interval time between calls to update the sensor data.
-intervalTime = Watch(5)  # Initial value of 30 seconds intervals
+intervalTime = Watch(30)  # Initial value of 30 seconds intervals
 # Global variable holding the reference to the interval loop
 intervalTimerRef = None
 # GLobal variable to keep track of the threshold value of the highest temperature.
@@ -42,18 +41,24 @@ ErrorPublisher = Publisher('IOTP/grp4/channel/error', on_connect=True)
 # Make a Subscription to the Command topic with the Default handler for the on_connect event used
 command_pipe = Subscription('IOTP/grp4/channel/cmnd', on_connect=True)
 
+timed
 
 
+
+
+
+""" Every time the mode changes with, execute/call the init function of that mode. """
 # Callback function that runs the operating mode initialization functions when mode changes
 def change_mode(mode):
     if mode == 'auto':
         mode_auto()
     elif mode == 'man':
         mode_man()
+    elif mode == 'timed':
+        mode_timed(args)
     else:
         # Create the error message
-        # Err_Msg = f"Invalid mode is being passed: {mode}"
-        Err_Msg = "Invalid mode is being passed: " + mode
+        Err_Msg = f"Invalid mode is being passed: {mode}"
         # Publish the error message
         ErrorPublisher < Err_Msg
         # Print/Log error
@@ -61,77 +66,97 @@ def change_mode(mode):
         # Return false to indicate error and operation failure
         return False
     # If there was no error, publish this change of mode as an event
-    Event < ("Mode: " + mode)
+    Event < f"Mode: {mode}"
     # Return True to indicate operation success
     return True
 
-
-# Callback function that runs when command received from command pipeline is 'ac'
+# Command function that runs when command received from command pipeline is 'ac'
 def ac_state(state):
     if state == 'on':
         ac.on()
     elif state == 'off':
         ac.off()
+    elif state == 'on x':
+        # Where x is the time to be on for
+        ac.on(x)
     else:
         # Print/Log error
         print('Invalid AC state received')
         # Return false to indicate error and operation failure
         return False
     # Return True to indicate operation success
-    return True
+    return False
 
 
-# Callback function to change interval time variable when command is intervalTime
+# Function to change interval time variable via MQTT
 def setIntervalTime(time):
     # Set the interval time and let the callback functions deal with the rest
     intervalTime < time
+
+
+# Callback function that deals with a time input from the MQTT broker
+def set_time(time):
+    # Check the current operating mode.
+    # If the mode is man
+    if operating_mode == 'man':
+        # Change the mode to timed and pass in the timme
+        operating_mode
+    elif state == 'timed':
+        # If the mode is 'timed' then restart the timer countdown with the new time
+        
+    elif state == 'on x':
+        # Where x is the time to be on for
+        ac.on(x)
+    else:
+        # Print/Log error
+        print('Invalid AC state received')
+        # Return false to indicate error and operation failure
+        return False
     # Return True to indicate operation success
-    return True
+    return False
+
 
 
 # Dictionary that holds all the functions for the given commands
 dispatch = {
     "mode": change_mode,
     "ac": ac_state,
-    "intervalTime": setIntervalTime
+    "interval time": setIntervalTime
+    "time": set_time
 }
 
 
-# Callback function used to parse messages/payload from the MQTT broker
-def parseMsg2(msg):
-    """
-    msg are concatenations of kv pair(s)
-    Seperate the kv pairs first
+# Function used to do a basic clean on strings for further processing
+def clean_string(str):
+    # Strip all the white spaces
+    if isinstance(str, list):
+        for index, item in enumerate(str):
+            str[index] = item.strip()
+        return str
+    else:
+        return str.strip()
+
+
+def parseMsg(msg):
+    # msg are concatenations of kv pair(s)
+    # Seperate the kv pairs first
     kv_pairs = msg.split(';')
-
-    Inner function for cleaning and formatting strings
-    def clean_string(str):
-        # Strip all the white spaces
-        if isinstance(str, list):
-            for index, item in enumerate(str):
-                str[index] = item.strip()
-            return str
-        else:
-            return str.strip()
-
-    Clean and format the input
+    # Clean and format the input
     kv_pairs = clean_string(kv_pairs)
-    Loop through all the key value pairs
+    
+    # Loop through all the key value pairs
     for kv in kv_pairs:
+        # Unpack the output from splitting the string as command and its arguements
+        command, args = kv.split(':')
 
         # Check if key exists in the dictionary first
         if command in dispatch:
-            # Run the command function with the args and check for its return statement
-            if not dispatch[command](args):
-                # If the operation failed
-                # Create the error message
-                Err_Msg = f"Invalid command received from MQTT Broker: {command} + {args}"
-                # Publish the error to the error topic
-                ErrorPublisher < Err_Msg
-                # Print/Log error
-                print(Err_Msg)
-                # Return false to indicate error and operation failure
-                return False
+            # Either return it or respond to it.
+            return dispatch[command](args)
+
+            # If the operation is a success
+            
+            pub(f"Invalid command received from MQTT Broker: {command} + {args}")
         else:
             # Key is not a valid command
             # Create the error message
@@ -141,32 +166,28 @@ def parseMsg2(msg):
             # Print/Log error
             print(Err_Msg)
             # Return false to indicate error and operation failure
-            return False """
-
-# Callback function used to parse messages/payload from the MQTT broker
-def parseMsg(msg):
-    # Unpack the output from splitting the string as command and its arguements
-    command, args = msg.split(':')
-
-    # Check if key exists in the dictionary first
-        # Run the command function with the args and check for its return statement
-    if (command in dispatch) and dispatch[command](args):
-            pass
-    else:
-        # Key is not a valid command
-        # Create the error message
-        Err_Msg = "Invalid command received from MQTT Broker: " + command + " " + args
-        # Publish the error to the error topic
-        ErrorPublisher < Err_Msg
-        # Print/Log error
-        print(Err_Msg)
-        # Return false to indicate error and operation failure
-        return False
+            return False
 
 
+parseMsg('mode:man; ac:on')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Function used to publish the new time.  -->  USe a decorator instead.
 def publish_int_time_change():
-    Event < ("Interval time updated to: " + intervalTime)
-# publish_int_time_change = lambda data: Event < f'Interval time successfully updated to: {data}'
+    Event < f'Interval time updated to: {intervalTime}'
+publish_int_time_change = lambda data: Event < f'Interval time successfully updated to: {data}'
 
 # Function used to restart the interval loop
 def restart_loop():
@@ -183,15 +204,17 @@ def readData():
     # Reference the variable in the global file scope, do I need this? Since I am only calling a method on the object
     global sensorData
     # Read data from sensor and store inside the sensorData object
-    sensorData(get_temp())
+    sensorData(BME.getData())
     # Publish the data to with the pre-defined Publisher
-    SensorData_Publisher < sensorData.value
+    SensorData_Publisher < sensorData
+
+# Look at the below module to see how to use
+import json
 
 
 """ The below functions are to run as "init" functions when the modes are first set.
     They will clear all callback functions and attach new ones for that particular mode.
 """
-# Init function for the auto operating mode
 def mode_auto():
     # Reference the global variable sensorData
     global sensorData
@@ -202,51 +225,95 @@ def mode_auto():
     def threshold_check(data):
         global ac
         # On the AC if temp exceeds threshold and it is off.
-        # if sensorData > threshold:
-        if sensorData.value > 26.0:
-            if not ac.state:
+        if sensorData > threshold:
+            if ac == 'off':
                 ac.on()
         # Off the AC if temp does not exceed threshold and it is on.
-        elif ac.state:
+        elif ac == 'on':
             ac.off()
 
     # The callback passed in to it runs every time the variable is changed whilst in auto mode.
     sensorData.on_change += threshold_check
 
-# Init function for the manual operating mode
+    # Below function is called on new command/msg. Function to return this inner function
+    def onMessage(msg):
+        pass
+        # Parse the message to determine what it is trying to say
+        # Verify that it is a valid message.
+        # If valid then do it
+        # else reject the message by publishing a 400 bad request?
+
+        # Make the below into a generator function that I can constantly yield new values out of.
+        # The yeild is to pause the execution of the function upon the so called "wait"
+
+
 def mode_man():
     """ Initialization function for the manual operating mode """
     # Reference the global variable sensorData
     global sensorData
     # Remove all eventHandlers / callbacks first before adding in callbacks for this mode.
     sensorData.clearAllListeners()
-    # No callbacks needed as manual mode just waits for input from the MQTT Broker or the Btn
+
+    # Below function is called on new command/msg. Function to return this inner function
+    def onMessage(msg):
+        pass
+        # Parse the message to determine what it is trying to say
+        # Verify that it is a valid message.
+        # If valid then do it
+        # else reject the message by publishing a 400 bad request?
+
+
+def mode_timed():
+    # Reference the global variable sensorData
+    global sensorData
+    # Remove all eventHandlers / callbacks first before adding in callbacks for this mode.
+    sensorData.clearAllListeners()
+
+    # On the ac for set amount of time.
+    ac.on(x)
+
+    # For the timed mode, listen for this few messages
+    # AC state change command
+    # Mode change command
+    # Set new time period/new timeout
 
 
 
-# This is the program's entry point. In last block to make sure all funcs and vars are defined prior to use
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Only run main code if module called as the program entry point.
+# Code in the last block to make sure all funcs and vars are defined prior to use.
 if __name__ == "__main__":
-    """ Attach all the callback functions defined above to their
-        respective Events and just wait for the Events to happen.
+    """ Attach callback functions defined above to the interval time variable
+    
     """
     # When the mode is changed, call the init switcher function.
     operating_mode.on_change += change_mode
 
     # Everytime the interval time is set, the Pi will produce and publish a new Event.
     intervalTime.on_set += restart_loop
-    # Everytime the interval times
-    intervalTime.on_change += publish_int_time_change
-
-    """ Test code """
-    # parseMsg('mode:man; ac:on')
-
 
     # Attach the parseMsg function as the callback handler for on_message events from the command pipeline
     command_pipe.on_message += parseMsg
+    
+
+
 
     # Start the manual mode as the initial default mode, by setting the mode to 'man'
-    # operating_mode < 'man'
-    operating_mode < 'auto'
+    operating_mode < 'man'
 
     # Start the loop to call readData function every "intervalTime" to update sensor Data on the global loop reference
     intervalTimerRef = setInterval(intervalTime.value, readData)
