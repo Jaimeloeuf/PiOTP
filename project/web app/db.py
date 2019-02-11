e2c866f1ae39e2564d14e0f91f6f3a4493a0a072
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-02-11 23:59:20 +0800

    Changes: 	Finnished the functions for DB usage and moved all the code into the 	db module inside the web-app dir, and removed the db2 module as it is 	no longer needed.

--- project/web app/db.py
+++ project/web app/db.py
@@ -1,45 +1,75 @@
 from influxdb import InfluxDBClient
+# Time module needed to create time stamps
+import time
 
-# Create a client that connects to the DB over HTTP
-client = InfluxDBClient(host='localhost', port=8086, username='root', password='root', database='SensorDB')
+
+# Initialization function
+def init():
+    # Below are the hardcoded credentials of the DB. To be changed!
+    USER = 'root'
+    PASSWORD = 'root'
+    DBNAME = 'sensordata'
+    HOST = 'localhost'
+    PORT = 8086
+
+    # Global variable to store the client object created by this init function
+    global client
+    # Create and attach the client object that connects to the DB over HTTP
+    client = InfluxDBClient(host='localhost', port=8086, username='root', password='root', database='SensorDB')
+
 
 # Get the list of databases in InfluxDB as a list of dictionaries
 def get_dbs():
-	return client.get_list_database()
+    return client.get_list_database()
 
 def create_db(DB):
-	# Instruct the DB to create a DB named 'SensorDB'
-	client.create_database(DB)
-
-query = 'select value from cpu_load_short;'
-json_body = [
-	{
-		"measurement": "cpu_load_short",
-		"tags": {
-			"host": "server01",
-			"region": "us-west"
-		},
-		"time": "2009-11-10T23:00:00Z",
-		"fields": {
-			"Float_value": 0.64,
-			"Int_value": 3,
-			"String_value": "Text",
-			"Bool_value": True
-		}
-	}
-]
+    # Lookup and loop through all existing databases
+    for db in get_dbs():
+        # If the database with the same name already exists
+        if DB == db:
+            # Return False to indicate operation failure and skip creation
+            return False
+    
+    # Else if none exists, instruct the DB to create a DB named 'SensorDB'
+    client.create_database(DB)
+    # Return True to indicate operation success
+    return True
 
 
 # Function to write data to the DataBase. DB defaults to SensorDB
 def write(data, DB='SensorDB'):
-	client.write_points
+    # Construct the data body that will be stored into the DB. To format it better!
+    pointValues = [
+        {
+            "time": time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()),
+            "measurement": 'reading1',
+            "tags": {
+                "nodeId": "node_1",
+            },
+            "fields": {
+                "value": data
+            },
+        }
+    ]
+    # Send data to DB to store
+    client.write_points(pointValues)
 
 
-
-# Ask client to close the HTTP connection to the DB server
-client.close()
+def disconnect():
+    # Referencing the global client variable
+    global client
+    # If connected to DB and client object is valid
+    if client != None:
+        # Ask client to close the HTTP connection to the DB server
+        client.close()
+    # Reset the reference to the client object. Init func needs to be called again to use the DB
+    client = None
 
 
 if __name__ == "__main__":
-	# Create the DB
-	create_db('SensorDB')
+    # Example/test code if ran as main module
+
+    # Create the DB
+    create_db('SensorDB')
+
+    # query = 'select value from cpu_load_short;'
